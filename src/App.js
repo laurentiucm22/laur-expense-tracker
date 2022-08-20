ea905a350c328254072e8ba4510792e09e60c3f0
Laurentiu CM <laurentiu.costachescu22@gmail.com>
2022-08-21 00:30:12 +0300

    Change location of DUMMY_EXPENSES

--- src/App.js
+++ src/App.js
@@ -2,33 +2,7 @@ import React, { useState } from "react";
 
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
-
-const DUMMY_EXPENSES = [
-  {
-    id: "e1",
-    title: "New Computer",
-    amount: 2022.2,
-    date: new Date(2022, 7, 14),
-  },
-  {
-    id: "e2",
-    title: "New TV",
-    amount: 799.49,
-    date: new Date(2020, 2, 12),
-  },
-  {
-    id: "e3",
-    title: "Toilet Paper",
-    amount: 94.12,
-    date: new Date(2021, 6, 22),
-  },
-  {
-    id: "e4",
-    title: "New Desk (Wooden)",
-    amount: 450.55,
-    date: new Date(2021, 5, 1),
-  },
-];
+import { DUMMY_EXPENSES } from "./constant/dummyExoenses";
 
 const App = () => {
   const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
